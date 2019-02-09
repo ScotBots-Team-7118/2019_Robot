8ccc8ae59259c1535574ff1ae6dd26f90523e1c6
AwesomeMP8 <ihascrudmail@gmail.com>
2019-02-09 14:53:05 -0800

    Added comments on DriveBase
    Next Step: SmartDashboard/Shuffleboard implementation in Robot.java.

--- src/main/java/frc/robot/DriveBase.java
+++ src/main/java/frc/robot/DriveBase.java
@@ -20,7 +20,7 @@ public class DriveBase {
     private final int[] DRIVE_TALON_PORT = {0, 0, 0, 0, 0};
 
     // Minimum joystick movement required for robot control
-    private final int JOYSTICK_DEADZONE = 0;
+    private final double JOYSTICK_DEADZONE = 0.1;
 
     // Drive speed for autonomous movement
     private final int AUTO_DRIVE_SPEED = 0;
@@ -31,19 +31,19 @@ public class DriveBase {
     private final double kI = 0;
     private final double kD = 0;
 
-    // Methods for turn method
+    // Variables for the turn method
     private final double TURN_OFFSET = 0;
     private final double MIN_TURN_SPEED = 0;
     private final double MAX_TURN_SPEED = 0;
     private final double MIN_DEGREES_FULL_SPEED = 0;
 
-    // The number of encoder rotations in a single food
+    // The number of encoder rotations in a single foot
     private final int ROTATIONS_TO_FEET = 0;
 
     // Maximum speed at which the drive train is allowed to move
-    private final int MAXIMUM_DRIVE_TALON_INPUT = 0;
+    private final double MAXIMUM_DRIVE_TALON_INPUT = 0.9;
 
-    //Stores encoder position
+    // The encoder positions from the last time they were reset
     private double initEncLeft, initEncRight, initEncCenter;
 
 
@@ -53,26 +53,22 @@ public class DriveBase {
      * @param Gyroscope gyro
      */
     public DriveBase(Gyroscope gyro) {
-        //Get instance of gyro
+        // Stores the correct instance of the gyroscope in the class for reference
         this.gyro = gyro;
-        //initLizes drive talons
+
+        // Drive Talon Initialization
         talLM = new TalonSRX(DRIVE_TALON_PORT[0]);
         talLF = new TalonSRX(DRIVE_TALON_PORT[1]);
         talRM = new TalonSRX(DRIVE_TALON_PORT[2]);
         talRF = new TalonSRX(DRIVE_TALON_PORT[3]);
         talC = new TalonSRX(DRIVE_TALON_PORT[4]);
        
-        //Pairs encoders with talons
-        talLM.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,0);
-        talRM.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,0);
-        talC.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,0 );
-        
-        //Allows for set inverted 
-        talLM.setSensorPhase(true);
-        talRM.setSensorPhase(true);
-        talC.setSensorPhase(true);
+        // Pairs encoders with their respective Talons
+        talLM.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+        talRM.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+        talC.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
 
-        //Gets encdoer value for future reference in getNormalizedPosition 
+        // Gets encdoer value for future reference in getNormalizedPosition 
         initEncLeft = talLM.getSelectedSensorPosition(0);
         initEncRight = talRM.getSelectedSensorPosition(0);
         initEncCenter = talC.getSelectedSensorPosition(0);
@@ -83,7 +79,6 @@ public class DriveBase {
      * @return
      */
     public double getNormalizedPositionL() {
-
         return talLM.getSelectedSensorPosition(0) - initEncLeft;
     }
         
@@ -92,7 +87,6 @@ public class DriveBase {
      * @return
      */
     public double getNormalizedPositionR() {
-
         return talRM.getSelectedSensorPosition(0) - initEncRight;
     }
 
@@ -101,7 +95,6 @@ public class DriveBase {
      * @return
      */
     public double getNormalizedPositionC() {
-
         return talC.getSelectedSensorPosition(0) - initEncCenter;
     }
         
@@ -109,7 +102,6 @@ public class DriveBase {
      * Resets the distance traveled by the encoders for the normalized positions.
      */
     public void resetEncoders() {
-
         initEncLeft = talLM.getSelectedSensorPosition(0);
         initEncRight = talRM.getSelectedSensorPosition(0);
         initEncCenter = talC.getSelectedSensorPosition(0);
@@ -129,15 +121,23 @@ public class DriveBase {
      * @param v
      */
     public void setLeft(double v) {
+        // Checks if the input is out of bounds, and returns out of the method if it is
         if (Math.abs(v) > 2) {
             System.out.println("Left side velocity out of range!!");
             return;
         }
 
-        if (Math.abs(v) > 0.9) {
-            v = 0.9 * (v / Math.abs(v));
+        // Makes sure the input velocity isn't above the maximum allowed velocity
+        if (Math.abs(v) > MAXIMUM_DRIVE_TALON_INPUT) {
+            v = MAXIMUM_DRIVE_TALON_INPUT * (v / Math.abs(v));
+        }
+        // Makes sure the input velocity isn't less than the joystick deadzone
+        else if (Math.abs(v) < JOYSTICK_DEADZONE) {
+            // If it is, set it to 0
+            v = 0;
         }
 
+        // Set the left-side talons to the new adjusted velocity
         talLM.set(ControlMode.PercentOutput, v);
         talLF.set(ControlMode.Follower, DRIVE_TALON_PORT[0]);
     }
@@ -148,16 +148,25 @@ public class DriveBase {
      * @param v
      */
     public void setRight(double v) {
-        if (Math.abs(v) > JOYSTICK_DEADZONE) {
+        // Checks if the input is out of bounds, and returns out of the method if it is
+        if (Math.abs(v) > 2) {
             System.out.println("Right side velocity out of range!!");
             return;
         }
-        if (Math.abs(v) > 0.9) {
-            v = 0.9 * (v / Math.abs(v));
+
+        // Makes sure the input velocity isn't above the maximum allowed velocity
+        if (Math.abs(v) > MAXIMUM_DRIVE_TALON_INPUT) {
+            v = MAXIMUM_DRIVE_TALON_INPUT * (v / Math.abs(v));
         }
+        // Makes sure the input velocity isn't less than the joystick deadzone
+        else if (Math.abs(v) < JOYSTICK_DEADZONE) {
+            // If it is, set it to 0
+            v = 0;
+        }
+
+        // Set the right-side talons to the new adjusted velocity
         talRM.set(ControlMode.PercentOutput, v);
         talRF.set(ControlMode.Follower, DRIVE_TALON_PORT[2]);
-
     }
 
     /**
@@ -166,15 +175,24 @@ public class DriveBase {
      * @param v
      */
     public void setCenter(double v) {
-        if (Math.abs(v) > JOYSTICK_DEADZONE) {
+        // Checks if the input is out of bounds, and returns out of the method if it is
+        if (Math.abs(v) > 2) {
             System.out.println("Right side velocity out of range!!");
             return;
         }
-        if (Math.abs(v) > 0.9) {
-            v = 0.9 * (v / Math.abs(v));
-        }
-        talC.set(ControlMode.PercentOutput, v);
 
+        // Makes sure the input velocity isn't above the maximum allowed velocity
+        if (Math.abs(v) > MAXIMUM_DRIVE_TALON_INPUT) {
+            v = MAXIMUM_DRIVE_TALON_INPUT * (v / Math.abs(v));
+        }
+        // Makes sure the input velocity isn't less than the joystick deadzone
+        else if (Math.abs(v) < JOYSTICK_DEADZONE) {
+            // If it is, set it to 0
+            v = 0;
+        }
+
+        // Set the center talon to the new adjusted velocity
+        talC.set(ControlMode.PercentOutput, v);
     }
 
     /**
@@ -191,14 +209,14 @@ public class DriveBase {
      * @param brake
      */
     public void brakeMode(boolean brake) {
-        // set talon brakemodes
-        if(brake){
+        if (brake) {
             talLM.setNeutralMode(NeutralMode.Brake);
             talLF.setNeutralMode(NeutralMode.Brake);
             talRM.setNeutralMode(NeutralMode.Brake);
             talRF.setNeutralMode(NeutralMode.Brake);
             talC.setNeutralMode(NeutralMode.Brake);
-        }else{
+        }
+        else {
             talLM.setNeutralMode(NeutralMode.Coast);
             talLF.setNeutralMode(NeutralMode.Coast);
             talRM.setNeutralMode(NeutralMode.Coast);
@@ -208,26 +226,26 @@ public class DriveBase {
     }
 
     /**
-     * format joystick inputs into an array
+     * Formats the joystick axis values into an array for teleop drive.
      * @param x
      * @param y
      * @param z
-     * @return
+     * @return A formatted double array (x-axis, y-axis, z-axis).
      */
     public double[] formatDriveJoystick(double x, double y, double z) {
+        // NOTE: Z-axis likely not needed, maybe should remove later
         double[] joyArray = {x, y, z};
         return joyArray;
     }
 
     /**
      * Drives the robot according to a single joystick
-     * with 360 degrees of freedom.
+     * with two axes of linear freedom (x and y).
      * @param joyR
      * @param joyL
      */
     public void teleopDrive(double[] joyR, double[] joyL) 
     {
-        //set up array of joy valus to input into drive methods
         double[] spinReturn = {joyL[2], joyR[2],((joyR[1] + joyL[1])/2)};
 
         //input joy commands into drive methods w/ cubic function
@@ -235,20 +253,17 @@ public class DriveBase {
         setLeft(Math.pow(spinReturn[1], 3));
         setCenter(Math.pow(spinReturn[2], 3));
     }
-    
-    // /**
-    //  * Turns the robot a given number of degrees at a set speed.
-    //  * @param angle
-    //  * @return
-    //  */
-    // public boolean turn(double angle){
-        
-    // }
 
     /**
-     * Enables PID control using set values of kF, kP, kI, and kD.
+     * Enables or disables PID control using set values of kF, kP, kI, and kD.
+     * @param mode
      */
-    public void enablePIDControl() {
-        
+    public void pidControl(boolean mode) {
+        if (mode) {
+            
+        }
+        else {
+
+        }
     }
 }
